95ed9d58ee71115ff26540285aa87cbb760c456d
Jimegroxak <noahpetz@gmail.com>
2025-06-05 15:06:13 -0400

    completed exercises 6-8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args = Array.from(arguments);
+    args.shift(); //remove array from args
+
+    for (arg of args) {
+        array = array.filter((value) => value !== arg );
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
